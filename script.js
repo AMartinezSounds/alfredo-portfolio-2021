dc160a89ed4a202760d5d93bea3c63b8fd97ece1
Alfredo Martínez <alfre_canos13@icloud.com>
2021-04-29 11:19:21 +0200

    fixing bugs

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 "strict mode";
 
-const navbarSquare = document.getElementById("navbar__lines");
-const lineMenu1 = document.getElementById("lines__line-1");
-const lineMenu2 = document.getElementById("lines__line-2");
-const lineMenu3 = document.getElementById("lines__line-3");
+const navbarSquare = document.querySelector(".navbar__lines");
+const lineMenu1 = document.querySelector(".lines__line-1");
+const lineMenu2 = document.querySelector(".lines__line-2");
+const lineMenu3 = document.querySelector(".lines__line-3");
 const navBar = document.querySelector(".navbar__links");
 const navBarLinks = document.querySelectorAll(".navbar__links--link");
 
@@ -13,4 +13,10 @@ navbarSquare.addEventListener("click", () => {
   lineMenu2.classList.toggle("change2");
   lineMenu3.classList.toggle("change3");
   navBar.classList.toggle("showNavbar");
+});
+
+navbarSquare.addEventListener("click", () => {
+  navbarSquare.classList.toggle("border-white");
+  lineMenu1.classList.toggle("line-black");
+  lineMenu3.classList.toggle("line-black");
 });
